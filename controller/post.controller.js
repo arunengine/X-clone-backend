abc3fa00779eeeefd3e75271d2143a26ae084e44
arunengine <iarunarumugam@gmail.com>
2026-06-28 12:21:42 +0530

    Fix deletePost: use cloudinary.uploader.destroy instead of cloudinary.destroy

--- controller/post.controller.js
+++ controller/post.controller.js
@@ -56,7 +56,7 @@ export const deletePost = async (req , res ) =>{
 
         if(post.img){
             const imgId = post.img.split("/").pop().split(".")[0]
-            await cloudinary.destroy(imgId)
+            await cloudinary.uploader.destroy(imgId)
         }
 
         await Post.findByIdAndDelete({ _id : id})
